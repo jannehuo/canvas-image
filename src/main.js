ad7b6e5ed03054c662408f10a811644d14aab8a3
Janne <EXT-Janne.Huotari@tulli.fi>
2019-01-25 15:32:49 +0200

    draw circles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { CanvasSpace } from 'pts'
+import { CanvasSpace, Pt, Group, Num, Rectangle } from 'pts'
+import { times } from 'lodash'
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
@@ -6,8 +7,28 @@ const screen = {
   w: window.innerWidth,
   h: window.innerHeight
 }
+const points = []
+const ratio = 20
 canvas.width = screen.w
 canvas.height = screen.h
 
 let space = new CanvasSpace( "#canvas" );
+let form = space.getForm()
 space.setup({ bgcolor: "#7d9cce" })
+
+const createPoints = () => {
+  times(screen.w, (w) => {
+    times(screen.h, (h) => {
+      if(w % ratio === 0 && h % ratio === 0) {
+        points.push(new Pt(w,h))
+      }
+    })
+  })
+}
+
+space.add( (time, ftime) => {
+  points.forEach(p => form.point(p, 1, 'circle').fill("#333").stroke('transparent'))
+});
+
+createPoints()
+space.play()
