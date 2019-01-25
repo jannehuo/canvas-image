dfa4f825358c536b6dcd07959d6f94bd78624378
Janne <EXT-Janne.Huotari@tulli.fi>
2019-01-25 13:36:16 +0200

    added pts

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { CanvasSpace } from 'pts'
+
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 const screen = {
@@ -6,3 +8,6 @@ const screen = {
 }
 canvas.width = screen.w
 canvas.height = screen.h
+
+let space = new CanvasSpace( "#canvas" );
+space.setup({ bgcolor: "#7d9cce" })
